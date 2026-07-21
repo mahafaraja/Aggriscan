e71d5c2d7a834e12f962a69c73325265c145e1a0
maha faraja <mahafaraja@gmail.com>
2026-07-22 02:02:36 +0300

    fixed assets path

--- frontend/src/components/AnimatedSplashScreen.tsx
+++ frontend/src/components/AnimatedSplashScreen.tsx
@@ -11,10 +11,10 @@ SplashScreen.preventAutoHideAsync();
 
 export function AnimatedSplashScreen({ onAnimationComplete }: { onAnimationComplete: () => void }) {
   const [fontsLoaded] = useFonts({
-    'Poppins-Regular': require('../assets/fonts/Poppins-Regular.ttf'),
-    'Poppins-Medium': require('../assets/fonts/Poppins-Medium.ttf'),
-    'Poppins-SemiBold': require('../assets/fonts/Poppins-SemiBold.ttf'),
-    'Poppins-Bold': require('../assets/fonts/Poppins-Bold.ttf'),
+    'Poppins-Regular': require('../../assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-Medium': require('../../assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-SemiBold': require('../../assets/fonts/Poppins-SemiBold.ttf'),
+    'Poppins-Bold': require('../../assets/fonts/Poppins-Bold.ttf'),
   });
 
   const fadeAnim = useRef(new Animated.Value(0)).current;
